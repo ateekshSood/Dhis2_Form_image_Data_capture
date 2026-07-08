cc85881944ec9e9836cbe22a524c3a14f8437803
Ateeksh <ateesood@gmail.com>
2026-07-08 19:56:12 +0000

    added session id verifying and added the route to view the datasets

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI , HTTPException
+from fastapi import FastAPI , HTTPException , Header , Depends
+from typing import Annotated
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import httpx
@@ -8,6 +9,25 @@ import secrets
 import time
 
 app = FastAPI()
+
+SESSION_TTL_SECONDS = 3600
+
+async def verify_session(authorization : Annotated[str | None, Header()] = None):
+
+    if authorization is not None and authorization.startswith("Bearer "):
+
+        session_id = authorization.split(" ")[1]
+        if session_id not in cookiesDict:
+            raise HTTPException(status_code = 401 , detail="you are not authorized")
+
+        current_time = time.time()
+        if current_time - cookiesDict[session_id][1] > SESSION_TTL_SECONDS:
+            raise HTTPException(status_code = 401 , detail="your session has expired please log in again")
+
+        return cookiesDict[session_id][0]
+
+    raise HTTPException(status_code=401 , detail="you are not authorized")
+            
 
 origins=[
     "http://localhost:5173"
@@ -48,6 +68,22 @@ async def sendCredentials(credentials : Credentials):
         
     
         return session_id
+
+
+@app.get("/datasets")
+async def fetchDatasets(session_cookie : Annotated[httpx.Cookies , Depends(verify_session)]):
+
+    async with httpx.AsyncClient() as client:
+
+        url = str(os.getenv("base_url")) + "/dataSets"
+        r = await client.get(url , cookies=session_cookie ,params={"fields": "id,name"})
+
+        if r.status_code != 200:
+            raise HTTPException(status_code = r.status_code , detail="Something went wrong in fetching datasets" )
+
+    return r.json()["dataSets"]
+            
+    
         
 @app.delete("/logout/{session_id}")
 def logout(session_id : str):
