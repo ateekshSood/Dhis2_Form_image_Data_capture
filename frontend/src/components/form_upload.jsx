54280743456219c104d0c1ded752a26c5eb5fce2
Ateeksh <ateesood@gmail.com>
2026-07-11 16:34:54 +0000

    kinda made the form uploading fronend not complete yeah :p

---
+++ frontend/src/components/form_upload.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import axios from "axios";
+
+function FormUpload() {
+
+    const [file, setFile] = useState(null);
+    const [errorMessage, setError] = useState("");
+    
+    async function fileUpload() {
+
+        if (file === null) {
+            setError("Please upload a file");
+            
+        }
+        else {
+          
+          const formData = new FormData();
+          formData.append("form", file);
+  
+          try {
+              const res = await axios.post("", formData);
+              setError("");
+          }
+          catch (err) {
+              if (err.response === undefined) {
+                  setError("Something went wrong");
+              }
+              else {
+                  setError(err.response.data.detail);
+              }
+          }
+          
+        }
+       
+  }
+
+
+    
+    return (
+        <div className="min-h-screen flex flex-col bg-[#1d5288]">
+
+          <header className="flex justify-end items-center  h-auto p-4">
+
+              <div>
+
+                  <a href="https://github.com/ateekshSood/Dhis2_Form_image_Data_capture.git" target="_blank" rel="noopener noreferrer"
+                      className="inline-block opacity-100 transition-opacity duration-200 hover:opacity-70">
+                      <img src="GitHub_Invertocat_Black.svg" alt="Github"
+                        className="h-8 w-8"></img>
+                  </a>
+
+              </div>
+
+            </header>
+
+            <main className="flex-1 flex flex-col justify-center items-center gap-3">
+                 {errorMessage && <div className="text-red-500 font-bold">{errorMessage}</div>}
+
+                <label className="text-white text-3xl mb-2" htmlFor="file_upload">Select your form : </label>
+                <input id="file_upload" type="file" placeholder="Choose Your File"
+                    onChange={(e) => setFile(e.target.files[0])} 
+                />
+
+                <button className="rounded-full px-6 py-2 bg-white text-black mt-2 hover:bg-gray-100 transition-colors"
+                    type="submit"
+                    onClick={fileUpload}
+                >Submit</button>
+
+
+            </main>
+
+          
+
+        </div>
+    )
+}
+
+export default FormUpload;
