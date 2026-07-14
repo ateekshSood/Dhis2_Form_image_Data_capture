7c8172fcfb9b04eb588f34e992416ceddddb8d94
Ateeksh <ateesood@gmail.com>
2026-07-14 18:51:50 +0000

    updated the main app.jsx to pass datasset as a prop to the form upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,21 @@
 import LoginForm from './components/login_form';
 import DatasetView from './components/datasets_view';
+import FormUpload from './components/form_upload';
 import { useState } from 'react';
+
 
 function App() {
 
     const [view, setView] = useState("login");
+    const [dataset, setDataset] = useState("");
 
 
     return (
     <>
     
             {view === "login" && <LoginForm onLoginSuccess={() => setView("datasets")} />}
-            {view === "datasets" && <DatasetView/>}
+            {view === "datasets" && <DatasetView onSelectSuccess={() => setView("form_upload")} onSetDataset = {(dataset)=>setDataset(dataset)} />}
+            {view === "form_upload" && <FormUpload selectedDataset={dataset}/>}
             
 
     </>
