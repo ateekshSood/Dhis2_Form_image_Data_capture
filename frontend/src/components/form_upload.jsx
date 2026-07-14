7598f2597d79245b9babcbcdf26703d135dda63b
Ateeksh <ateesood@gmail.com>
2026-07-14 18:50:54 +0000

    made the form uplaod part compelteing and it is receving a storing id from the backend in the sessino storage

--- frontend/src/components/form_upload.jsx
+++ frontend/src/components/form_upload.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-function FormUpload() {
+function FormUpload({selectedDataset}) {
 
     const [file, setFile] = useState(null);
     const [errorMessage, setError] = useState("");
@@ -15,10 +15,17 @@ function FormUpload() {
         else {
           
           const formData = new FormData();
-          formData.append("form", file);
+            formData.append("file", file);
+            formData.append("dataset" , selectedDataset)
   
           try {
-              const res = await axios.post("", formData);
+              const session_id = sessionStorage.getItem("session_id");
+              const res = await axios.post("http://127.0.0.1:8000/upload", formData, {
+                  headers: {
+                      'Authorization': 'Bearer ' + session_id,
+                  }
+              });
+              sessionStorage.setItem("upload_id", res.data)
               setError("");
           }
           catch (err) {
@@ -59,6 +66,12 @@ function FormUpload() {
                 <label className="text-white text-3xl mb-2" htmlFor="file_upload">Select your form : </label>
                 <input id="file_upload" type="file" placeholder="Choose Your File"
                     onChange={(e) => setFile(e.target.files[0])} 
+                    className="text-sm text-white
+                            file:mr-4 file:py-2 file:px-4
+                            file:rounded-full file:border-0
+                            file:text-sm file:font-semibold
+                            file:bg-violet-50 file:text-black"
+                    capture="environment"
                 />
 
                 <button className="rounded-full px-6 py-2 bg-white text-black mt-2 hover:bg-gray-100 transition-colors"
