2f082f71ba7497be8ab6aef808d87b49801c25a2
Ateeksh <ateesood@gmail.com>
2026-07-16 08:43:35 +0000

    made the upload route

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI , HTTPException , Header , Depends
+from fastapi import FastAPI , HTTPException , Header , Depends , File , UploadFile , Form
+import filetype
+import anyio
 from typing import Annotated
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -11,23 +13,7 @@ import time
 app = FastAPI()
 
 SESSION_TTL_SECONDS = 3600
-
-async def verify_session(authorization : Annotated[str | None, Header()] = None):
-
-    if authorization is not None and authorization.startswith("Bearer "):
-
-        session_id = authorization.split(" ")[1]
-        if session_id not in cookiesDict:
-            raise HTTPException(status_code = 401 , detail="you are not authorized")
-
-        current_time = time.time()
-        if current_time - cookiesDict[session_id][1] > SESSION_TTL_SECONDS:
-            raise HTTPException(status_code = 401 , detail="your session has expired please log in again")
-
-        return cookiesDict[session_id][0]
-
-    raise HTTPException(status_code=401 , detail="you are not authorized")
-            
+UPLOAD_DIR = "UPLOAD_DIR"
 
 origins=[
     "http://localhost:5173"
@@ -46,6 +32,24 @@ class Credentials(BaseModel):
 
 load_dotenv()
 cookiesDict = {}
+uploadsDict = {}
+
+async def verify_session(authorization : Annotated[str | None, Header()] = None):
+
+    if authorization is not None and authorization.startswith("Bearer "):
+
+        session_id = authorization.split(" ")[1]
+        if session_id not in cookiesDict:
+            raise HTTPException(status_code = 401 , detail="you are not authorized")
+
+        current_time = time.time()
+        if current_time - cookiesDict[session_id][1] > SESSION_TTL_SECONDS:
+            raise HTTPException(status_code = 401 , detail="your session has expired please log in again")
+
+        return cookiesDict[session_id][0]
+
+    raise HTTPException(status_code=401 , detail="you are not authorized")
+            
 
 @app.post("/Credentials/")
 async def sendCredentials(credentials : Credentials):
@@ -82,7 +86,61 @@ async def fetchDatasets(session_cookie : Annotated[httpx.Cookies , Depends(verif
             raise HTTPException(status_code = r.status_code , detail="Something went wrong in fetching datasets" )
 
     return r.json()["dataSets"]
+
+@app.post("/upload")
+async def uploadForm(file : UploadFile = File(...) , dataset : str = Form(...) , session_cookie : httpx.Cookies = Depends(verify_session) ):
+    CHUNK_SIZE = 1024 * 10
+    MAX_SIZE = 1024 * 1024 * 10
+
+    contents = b""
+    while True:
+        chunk = await file.read(CHUNK_SIZE)
+        if chunk == b"": #it returns empty byte obj when nothing is left to reaad can also use not chunk here 
+                        # but to remember it does that i used this
+            break
+
+        contents += chunk
+
+        if len(contents) > MAX_SIZE:
+            raise HTTPException(status_code = 413 , detail="File Size is too large to process")
+            # 413 = payload too large
             
+        
+
+    detected = filetype.guess(contents)
+
+    if detected is None:
+        raise HTTPException(status_code = 415 , detail = "Unsupported media type ")
+        # 415 = unsupporeted media type 
+        # 400 = bad requeset
+
+
+    ALLOWED_EXTENSIONS = ["jpg" , "png" , "pdf"]
+
+    if detected.extension not in ALLOWED_EXTENSIONS:
+        raise HTTPException(status_code = 415 , detail = "Unsupported Extension")
+        
+    upload_id = secrets.token_urlsafe()
+    filename = upload_id + "." + detected.extension
+
+    os.makedirs(UPLOAD_DIR , exist_ok=True)
+    
+    filepath = f"{UPLOAD_DIR}/{filename}"
+
+    async with await anyio.open_file(filepath , "wb") as f:
+        await f.write(contents)
+        
+    uploadsDict[upload_id] = (filepath , dataset , session_cookie) 
+
+    return upload_id
+
+    
+    
+    
+    
+            
+            
+    
     
         
 @app.delete("/logout/{session_id}")
