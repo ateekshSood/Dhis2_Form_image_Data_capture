dcf4d9af102839bc7903128e0b0a7c9fb55d6ba1
Ateeksh <ateesood@gmail.com>
2026-07-20 14:39:30 +0000

    complted the image preprocessing pipeline

--- preprocessing/image_processing.py
+++ preprocessing/image_processing.py
@@ -19,8 +19,6 @@ if image is not None:
     contours , hierarchy= cv2.findContours(edge , cv2.RETR_LIST , cv2.CHAIN_APPROX_SIMPLE) # return tuple (contours , hierarchy)
     biggest_contour = max(contours , key=cv2.contourArea)
 
-    x , y , w , h = cv2.boundingRect(biggest_contour)
-
     center , (width , height) , angle = cv2.minAreaRect(biggest_contour) 
 
     if angle < -45:
@@ -29,8 +27,17 @@ if image is not None:
 
     m = cv2.getRotationMatrix2D(center , angle , 1.0)
     rotated = cv2.warpAffine(image , m , image.shape[:2][::-1])
-    deskewed = cv2.getRectSubPix(rotated , image.shape[:2][::-1] , center) 
+    deskewed = cv2.getRectSubPix(rotated , (int(width) , int(height)) , center) 
     cv2.imwrite("images/deskewed_image.jpg" , deskewed)
+
+    gray_deskewed = cv2.cvtColor(deskewed , cv2.COLOR_BGR2GRAY)
+
+    denoise_image = cv2.fastNlMeansDenoising(gray_deskewed , h=5 , searchWindowSize=21, templateWindowSize=7)
+    cv2.imwrite("images/denoised_img.jpg" , denoise_image)
+
+    adaptive_threshold_image = cv2.adaptiveThreshold(denoise_image , 255 , cv2.ADAPTIVE_THRESH_GAUSSIAN_C , thresholdType=cv2.THRESH_BINARY , 
+        blockSize=15 , C = 2)
+    cv2.imwrite("images/adaptive_threshold_image.png" , adaptive_threshold_image)
 
   
     
