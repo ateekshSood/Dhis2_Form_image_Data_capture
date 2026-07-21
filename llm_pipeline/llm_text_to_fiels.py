a15899dcad5ebc83868085517462f246cd0a17c6
Ateeksh <ateesood@gmail.com>
2026-07-21 15:53:08 +0000

    made the llm pipeline for mapping fields to data elements

--- llm_pipeline/llm_text_to_fiels.py
+++ llm_pipeline/llm_text_to_fiels.py
@@ -1,41 +1,66 @@
 import json
 import os
-
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 openrouter_api_key = os.getenv("open_router_api_key")
 
-response = requests.post(
-    url="https://openrouter.ai/api/v1/chat/completions",
-    headers={
-        "Authorization": "Bearer " + str(openrouter_api_key),
-        "Content-Type": "application/json",
-    },
-    data=json.dumps(
-        {
-            "model": "google/gemma-4-31b-it:free",
-            "messages": [
-                {
-                    "role": "user",
-                    "content": "How many r's are there in word strawberry?",
-                }
-            ],
-            "reasoning": {"enabled": True},
-        }
-    ),
-)
+def llm_field_mapping(ocr_text, field_list):
 
-response_text = response.json()
-
-if "error" in response_text:
-    print("API Error:", response_text["error"]["metadata"]["raw"])
-
-else:
-    response_text = response_text["choices"][0]["message"]["content"]
-
-    print(response_text)
-
+    response = requests.post(
+        url="https://openrouter.ai/api/v1/chat/completions",
+        headers={
+            "Authorization": "Bearer " + str(openrouter_api_key),
+            "Content-Type": "application/json",
+        },
+        data=json.dumps(
+            {
+                # "model": "google/gemma-4-31b-it:free",
+                "model":"nvidia/nemotron-3-super-120b-a12b:free",
+                "messages": [
+                    {
+                        "role": "system",
+                        "content": "analyze the content given to you and map them to the given json_schema and output the response in that order strictly. If you are not sure about a field mapping output low confidence . if you are sure about field mapping ouput high confidence. Strictly only use the dataElementId that are provided in the text content.",
+                    },
+                    
+                    {
+                        "role":"user",
+                        "content": f"ocr text : {ocr_text} field list : {field_list}"
+                    }
+                ],
+                "response_format": {
+                    "type": "json_schema",
+                    "json_schema": {
+                        "name": "data_elements",
+                        "schema": {
+                            "type": "array",
+                            "items": {
+                                "type": "object",
+                                "properties": {
+                                    "dataElementId": {"type": "string"},
+                                    "value": {"type": "string"},
+                                    "confidence": {"type": "string", "enum": ["high", "low"]}
+                                },
+                                "required": ["dataElementId", "value", "confidence"]
+                            }
+                        },
+                        "strict":True
+                    }
+                },
+                "reasoning": {"enabled": True},
+            }
+        ),
+    )
+    
+    response_text = response.json()
+    
+    if "error" in response_text:
+        raise Exception(response_text["error"]["metadata"]["raw"])
+    
+    else:
+        response_text = response_text["choices"][0]["message"]["content"]
+    
+        return (json.loads(response_text))
 
 
