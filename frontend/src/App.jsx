4d6d2fa1c607773e2b37b54164fb8f74752038ea
Ateeksh <ateesood@gmail.com>
2026-07-24 16:41:36 +0000

    connected form field output with main file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import LoginForm from './components/login_form';
 import DatasetView from './components/datasets_view';
 import FormUpload from './components/form_upload';
+import Review from './components/review';
 import { useState } from 'react';
 
 
@@ -15,7 +16,8 @@ function App() {
     
             {view === "login" && <LoginForm onLoginSuccess={() => setView("datasets")} />}
             {view === "datasets" && <DatasetView onSelectSuccess={() => setView("form_upload")} onSetDataset = {(dataset)=>setDataset(dataset)} />}
-            {view === "form_upload" && <FormUpload selectedDataset={dataset}/>}
+            {view === "form_upload" && <FormUpload selectedDataset={dataset} onUploadSuccess={() => setView("review")} />}
+            {view === "review" && <Review/>}
             
 
     </>
